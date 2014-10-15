f3a147144bd244b694e7467a4fbfcf87347737d9
tbille <thomas.bille625@gmail.com>
2014-10-15 16:27:38 -0400

    Ajoute changement automatique de questions test rapide

--- bdd.js
+++ bdd.js
@@ -10,120 +10,120 @@ var tableauQuestions = [
 	id:1,
 	domaine:1,
 	question:'Choisissez le bon tag HTML pour créer une liste non ordonnée',
-	reponses:{
-		reponse1:'<li>',
-		reponse2:'<ul>',
-		reponse3:'<ol>',
-		reponse4:'<br>'
-	},
+	reponses:[
+		'<li>',
+		'<ul>',
+		'<ol>',
+		'<br>'
+	],
 	bonneReponse:2
 },
 {
 	id:2,
 	domaine:1,
 	question:'Quelle est la version précédant le HTML5',
-	reponses:{
-		reponse1:'HTML 4.01',
-		reponse2:'HTML 4.1',
-		reponse3:'HTML 4.9',
-		reponse4:'HTML 4'
-	},
+	reponses:[
+		'HTML 4.01',
+		'HTML 4.1',
+		'HTML 4.9',
+		'HTML 4'
+	],
 	bonneReponse:1
 },
 {
 	id:3,
 	domaine:1,
 	question:'Quel est le bon doctype pour HTML5',
-	reponses:{
-		reponse1:'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 5.0//EN" "http://www.w3.org/TR/html5/strict.dtd">',
-		reponse2:'<!DOCTYPE HTML5>',
-		reponse3:'<!DOCTYPE html>',
-		reponse4:'<!DOCTYPE PUBLIC html>',
-	},
+	reponses:[
+		'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 5.0//EN" "http://www.w3.org/TR/html5/strict.dtd">',
+		'<!DOCTYPE HTML5>',
+		'<!DOCTYPE html>',
+		'<!DOCTYPE PUBLIC html>',
+	],
 	bonneReponse:3
 },
 {
 	id:4,
 	domaine:1,
 	question:'Quel est le bon élément HTML5 pour la lecture des vidéos',
-	reponses:{
-		reponse1:'<media>',
-		reponse2:'<video>',
-		reponse3:'<play>',
-		reponse4:'<mediatype>'
-	},
+	reponses:[
+		'<media>',
+		'<video>',
+		'<play>',
+		'<mediatype>'
+	],
 	bonneReponse:2
 },
 {
 	id:5,
 	domaine:2,
 	question:'Quel est le bon endroit dans un document HTML pour placer la référence à une feuille de style externe?',
-	reponses:{
-		reponse1:'À la fin du document',
-		reponse2:'Dans la section <body>',
-		reponse3:'Dans la section <head>',
-		reponse4:'En haut du document'
-	},
+	reponses:[
+		'À la fin du document',
+		'Dans la section <body>',
+		'Dans la section <head>',
+		'En haut du document'
+	],
 	bonneReponse:3
 },
 {
 	id:6,
 	domaine:2,
 	question:'Lequel parmi les choix correspond à la bonne syntaxe?',
-	reponses:{
-		reponse1:'body:color=black;',
-		reponse2:'body {color: black;}',
-		reponse3:'{body:color=black;}',
-		reponse4:'{body;color:black;'
-	},
+	reponses:[
+		'body:color=black;',
+		'body {color: black;}',
+		'{body:color=black;}',
+		'{body;color:black;'
+	],
 	bonneReponse:2
 },
 {
 	id:7,
 	domaine:2,
 	question:'Comment insère-t-on un commentaire dans un fichier CSS?',
-	reponses:{
-		reponse1:'/* ceci est un commentaire */',
-		reponse2:'// ceci est un commentaire //',
-		reponse3:'<--! ceci est un commenaire -->',
-		reponse4:'$ ceci est un commentaire $'
-	},
+	reponses:[
+		'/* ceci est un commentaire */',
+		'// ceci est un commentaire //',
+		'<--! ceci est un commenaire -->',
+		'$ ceci est un commentaire $'
+	],
 	bonneReponse:1
 },
 {
 	id:8,
 	domaine:3,
 	question:'Dans quel élément HTML met-on le Javascript?',
-	reponses:{
-		reponse1:'<js>',
-		reponse2:'<java>',
-		reponse3:'<javascript>',
-		reponse4:'<script>'
-	},
+	reponses:[
+		'<js>',
+		'<java>',
+		'<javascript>',
+		'<script>'
+	],
 	bonneReponse:4
 },
 {
 	id:9,
 	domaine:3,
 	question:'Quelle est la bonne syntaxe pour référer à un script externe nommé "xxx.js"?',
-	reponses:{
-		reponse1:'<script src="xxx.js">',
-		reponse2:'<script>xxx.js</script>',
-		reponse3:'<js src="xxx.js">',
-		reponse4:'<javascript:"xxx.js">'
-	},
+	reponses:[
+		'<script src="xxx.js">',
+		'<script>xxx.js</script>',
+		'<js src="xxx.js">',
+		'<javascript:"xxx.js">'
+	],
 	bonneReponse:1
 },
 {
 	id:10,
 	domaine:3,
 	question:'Comment fait-on pour détecter le nom du navigateur du client?',
-	reponses:{
-		reponse1:'navigator-name',
-		reponse2:'nav.name',
-		reponse3:'navigator.appName',
-		reponse4:'what.is.the.navigator.name'
-	},
+	reponses:[
+		'navigator-name',
+		'nav.name',
+		'navigator.appName',
+		'what.is.the.navigator.name'
+	],
 	bonneReponse:3
 },
 ];
@@ -165,14 +165,16 @@ var getNameDomaineFromID = function (id){
 
 /*
 	Fonctions qui retourne un tableau de question du domaine de l'identifiant	
-	Entrée : identifiant domaine:integer
+	Entrée : identifiant domaine:integer array
 	Sortie : tableau de questions:object array (id,domaine,question,reponse[1-4],bonneReponse)
 */
-var getQuestionsFromDomaine= function (idDomaine){
+var getQuestionsFromDomaine= function (tableauidDomaine){
 	var tableauRetour=[];
 	for(i=0;i<tableauQuestions.length;i++){
-		if(tableauQuestions[i].domaine==idDomaine){
-			tableauRetour.push(tableauQuestions[i]);
+		for(j=0;j<tableauidDomaine.length;j++){
+			if(tableauQuestions[i].domaine==tableauidDomaine[j]){
+				tableauRetour.push(tableauQuestions[i]);
+			}
 		}
 	}
 	return tableauRetour;
