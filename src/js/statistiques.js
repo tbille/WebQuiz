6f57d683ae029af1e69585616e28dddf5002e7ef
tbille <thomas.bille625@gmail.com>
2014-10-24 18:04:06 -0400

    Ajoute fichier stats

---
+++ src/js/statistiques.js
@@ -0,0 +1,71 @@
+
+/*
+
+voila toutes les variables dont j'ai besoin au cours du site :
+
+objet test rapide: {
+	nombre de question test réussi : 0
+	nombre de question test : 0
+}
+
+tableau d'examen[
+
+{
+	id domaines [ id1 id2 ... ]
+	resultat    /20
+}
+{
+	id domaines [ id1 id2 ... ]
+	resultat    /20
+}
+{
+	id domaines [ id1 id2 ... ]
+	resultat    /20
+}
+{
+	id domaines [ id1 id2 ... ]
+	resultat    /20
+}
+{
+	id domaines [ id1 id2 ... ]
+	resultat    /20
+}
+
+]
+
+
+*/
+
+
+
+var isInitialise(){
+
+}
+
+initialiase()
+
+ajouterQuestionTest(true / false : reussi ou pas)
+	if true then
+		objet.nbReuss ++
+		objet.nbQuestion ++
+	else
+		objet.nbQuestion ++
+	end if
+end
+
+calculPourcentageTestRapid()
+	return objet.nbReuss / objet.nbQuestion * 100
+
+
+/**
+
+EXAMEN
+
+*/
+
+ajouteExamen(note)
+
+calculPourcentageExmen()
+
+getAllExams()
+	return tableau
